82f68608b2678d787ffc087da9c975c8afcff6c9
Andres Camilo Rios <acamilorm4556@gmail.com>
2023-06-19 18:03:38 -0500

    add and delete tasks

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [todoList, setTodoList] = useState([])
+  const [taskId, setTaskId] = useState(1)
+  const [input, setInput] = useState({id: 1, content: ""})
+
+  function handleChange(e) {
+    e.preventDefault()
+    setInput({...input,
+      id: taskId,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    if(input.content ===""){
+      alert("Por favor, escriba una tarea")
+    }else{
+      setTodoList([...todoList, input])
+      setInput({id: taskId + 1, content: ""})
+      setTaskId(taskId + 1)
+    }
+  }
+
+  function handleDelete(id){
+    setTodoList(todoList.filter(t => t.id !== id))
+  }
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <input 
+          type='text'
+          name='content'
+          value={input.content}
+          onChange={(e) => handleChange(e)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <div>
+        {todoList.length ? 
+        todoList.map((t) => (
+          <div key={t.id}>
+            <p>{t.content}</p>
+            <button value={t.id} onClick={() => handleDelete(t.id)}>Delete</button>
+          </div>
+        )):
+        (<div>Sin tareas</div>)
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
