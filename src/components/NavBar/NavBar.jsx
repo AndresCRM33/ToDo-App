babaadde92e2800e1e788fa4e65c26b7d5f94f7e
Andres Camilo Rios <acamilorm4556@gmail.com>
2023-07-22 15:21:57 -0500

    navbar prototype styles

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,8 +3,8 @@ import styles from "./NavBar.module.css"
 
 export function NavBar(){
     return(<div className={styles.container}>
-        <h1>ToDo App</h1>
-        <ul>
+        <h1 className={styles.logo}>ToDo App</h1>
+        <ul className={styles.menuNav}>
             <li>About</li>
             <li>ToDo List</li>
         </ul>
