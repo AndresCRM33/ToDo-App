865a2cfb72d217bdb91f6a6e362a94c7cca57bae
Andres Camilo Rios <acamilorm4556@gmail.com>
2023-07-23 22:42:11 -0500

    redux persist is working

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from './App.jsx'
 import './index.css'
 
 import { Provider } from 'react-redux'
-import store from "./redux/store/index.js"
+import { PersistGate } from 'redux-persist/integration/react'
+import store, {persistor} from "./redux/store/index.js"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,15 +1,28 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Importa el almacenamiento que deseas utilizar (por ejemplo, localStorage)
 import rootReducer from '../reducer/index.js';
 import thunk from 'redux-thunk';
 
 const composeEnhancers =
-   (typeof window !== 'undefined' &&
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-   compose;
+  (typeof window !== 'undefined' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+// Configuración de Redux Persist
+const persistConfig = {
+  key: 'root', // La clave raíz para el almacenamiento en el navegador
+  storage, // El almacenamiento que deseas utilizar
+  // Aquí puedes configurar más opciones, como las listas blancas o negras para persistir solo ciertas partes del estado
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = createStore(
-   rootReducer,
-   composeEnhancers(applyMiddleware(thunk))
+  persistedReducer,
+  composeEnhancers(applyMiddleware(thunk))
 );
+
+export const persistor = persistStore(store);
 
 export default store;
